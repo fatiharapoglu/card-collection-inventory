ed882b17a7d3934bdb113d2a2764abaa30c3ea15
Fatih Arapoglu <f3tt4n@gmail.com>
2023-02-27 21:40:55 +0300

    fixed hanging terminal error

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # expressInventory
 
 express
-have to learn express better to work on this
-watch all projects in the 18 hour fcc youtube node/express full course
-10 h one left
-3:07
-3:19 at original
-5~
--- server.js
+++ server.js
@@ -1,35 +1,7 @@
 const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
 require("dotenv").config();
-// const favicon = require("serve-favicon");
-
-const indexRouter = require("./routes/index");
-
 const app = express();
+
 const port = process.env.PORT || 3000;
-const mongoDB = process.env.MONGODB_URI;
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-// app.use(favicon(path.join(__dirname, "public/assets", "favicon.png")));
-
-app.use("/", indexRouter);
-
-mongoose.set("strictQuery", false);
-
-dbConnection();
-async function dbConnection() {
-    try {
-        await mongoose.connect(mongoDB);
-        app.listen(port, () => console.log(`Listening on port ${port}...`));
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-module.exports = app;
+app.listen(port, console.log(`Server listening on port ${port}...`));
