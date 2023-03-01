a33a6dd187963f4cd42c7cf5e1b17d54ecd4b6ee
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-01 04:57:19 +0300

    handle creating card errors

--- controllers/new.js
+++ controllers/new.js
@@ -1,8 +1,12 @@
 const Card = require("../models/card");
 
 const addCard = async (req, res) => {
-    const card = await Card.create(req.body);
-    res.status(201).render("card", { card });
+    try {
+        const card = await Card.create(req.body);
+        res.status(201).render("card", { card });
+    } catch (error) {
+        res.status(500).send(`Something went wrong. Error: ${error}`);
+    }
 };
 
 module.exports = addCard;
