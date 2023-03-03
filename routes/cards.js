d93d6160279b72ac922b5c4ba84c1eaf960b9081
Fatih Arapoglu <f3tt4n@gmail.com>
2023-03-03 22:56:40 +0300

    clean code

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.use(express.static("public"));
-
 const { getAllCards, getCard, deleteCard, editCard } = require("../controllers/cards");
+
+router.use(express.static("public"));
 
 router.route("/").get(getAllCards);
 router.route("/:id").get(getCard).patch(editCard).delete(deleteCard);
--- routes/new.js
+++ routes/new.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const addCard = require("../controllers/new");
 
 router.use(express.static("public"));
